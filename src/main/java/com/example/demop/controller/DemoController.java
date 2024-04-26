71f43b711c1150c6607aeb104914662d95f2d686
guptavandu2356 <163030125+guptavandu2356@users.noreply.github.com>
2024-04-26 14:59:16 +0530

    Update DemoController.java

--- src/main/java/com/example/demop/controller/DemoController.java
+++ src/main/java/com/example/demop/controller/DemoController.java
@@ -12,7 +12,7 @@ public class DemoController {
 	@GetMapping("/headline")
 	@ResponseBody
 	public String news() {
-		System.out.println("Change done");
+		
 		return "News app getting tested";
 	}
 
